a882dfc892e551d42557ae4e4d23f6983f901a53
kunzimariano <kunzi.mariano@gmail.com>
2014-08-12 14:13:03 -0300

    Initial commit.

---
+++ VersionOne.CorsProxy.js
@@ -0,0 +1,64 @@
+var bodyParser = require('body-parser');
+var request = require('request');
+var express = require('express'),
+    cors = require('cors'),
+    app = express();
+
+app.use(bodyParser.text({ type : 'application/xml' }));
+//TODO: make it work only with the requestor url
+app.use(cors());
+//TODO: make this configurable
+var port = '1234';
+
+function getUrl(url) {
+    //remove the initial slash
+    return url.substr(1, url.length - 1);
+}
+
+function getHeaders(headers) {
+    var result = {};
+    for (h in headers) {
+        if (h == 'host' || h == 'origin' || h == 'referer' || h == 'accept-encoding')
+            continue;
+        result[h] = headers[h];
+    }
+    return result;
+}
+
+function addHeaders(response, headers) {
+    for (h in headers) {
+        response.setHeader(h, headers[h]);
+    }
+}
+
+app.get('/*', function (req, res, next) {
+    var options = {
+        url: getUrl(req.url),
+        method: 'GET',
+        headers: getHeaders(req.headers)
+    };
+    
+    request(options, function (error, response, body) {
+        addHeaders(res, getHeaders(response.headers));
+        res.end(body);
+    });
+});
+
+app.post('/*', function (req, res, next) {
+    var options = {
+        url: getUrl(req.url),
+        method: 'POST',
+        body: req.body,
+        headers: getHeaders(req.headers)
+    };
+    
+    request(options, function (error, response, body) {
+        addHeaders(res, getHeaders(response.headers));
+        res.status(200).send(body);
+    });
+    
+});
+
+app.listen(port, function () {
+    console.log('CORS-enabled web server listening on port ' + port);
+});
